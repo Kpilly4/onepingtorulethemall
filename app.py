94a605a45a876aa30c4c2fcf5040b5549aca95e1
KennyP <ken@analogdigital.ca>
2022-03-24 21:15:31 -0600

    Removed Navigation

--- app.py
+++ app.py
@@ -12,9 +12,16 @@ def home():
         {"title": "Analog'Digtal", "url": "https://www.analogdigital.ca"},
         {"title": "ShiftConnect", "url": "https://www.shiftconnect.com"},
         {"title": "OneTwoPay", "url": "https://www.onetwopay.ca"},
-        {"title": "PayFromAway", "url": "https://www.payfromaway.ca/"},
+        {"title": "PayFromAway", "url": "https://www.payfromaway.ca"},
         {"title": "Debbie Pushor", "url": "https://www.debbiepushor.ca"},
-        {"title": "Canmore Folk Music Festival", "url": "https://www.canmorefolkfestival.com"}
+        {"title": "Canmore Folk Music Festival", "url": "https://www.canmorefolkfestival.com"},
+        {"title": "Bowda", "url": "https://www.bowda.ca"},
+        {"title": "De Lumber", "url": "https://www.delumber.com"},
+        {"title": "MindYrLife", "url": "https://www.mindyrlife.com"},
+        {"title": "Brewster's Mountain lodge", "url": "https://www.brewstermountainlodge.com"},
+        {"title": "The Edge Exotic", "url": "https://www.theedgeexotic.com"},
+
+
     ]
     return render_template("home.html", panels=panels)
 
